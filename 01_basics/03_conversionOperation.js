4f3e54ff20772ecf1d32af7c1987c41b3168ae98
Rubeesingh <136982220+Rubeesingh@users.noreply.github.com>
2024-03-23 07:26:39 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,8 +24,32 @@
 // "" => false
 // "rush" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************Operation **********
+
+
+let value = 5
+let negValue = -value
+console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = " sushi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
