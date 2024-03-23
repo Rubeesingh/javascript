13c3953d4bc60fe59b0f7aa8bd18708af18395d2
Rubeesingh <136982220+Rubeesingh@users.noreply.github.com>
2024-03-23 07:07:21 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,31 @@
-let score = '33abc'
-let scores = null
+// let score = '33abc'
+// let scores = null
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(scores);
+// let valueInNumber = Number(scores);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+// "33" => 33 easily conversion
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+
+
+// let isLoggedIn = "rushu"
+
+// let booleanIsLoggedIn =  Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0=> false
+// "" => false
+// "rush" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
