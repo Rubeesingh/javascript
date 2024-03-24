1418fe1a3cf2c7b44d1db52c843c40e6f0a78345
Rubeesingh <136982220+Rubeesingh@users.noreply.github.com>
2024-03-24 19:25:13 +0000

    some changes

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+const name = "roonee"
+const repoCount = 05
+
+// console.log(name +  repoCount + " value");
+// this is new techniqe to write string or add
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('rushi')
+
+console.log(gameName[0]);  // string ke letter acces krne ke liye hm aise likhte h
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.charAt('s'));
+
+
+
